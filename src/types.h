c7660dd75385d550471ad71b8dc7195ccd3af139
Alex Fedorov <fedorov1984@gmail.com>
2018-03-19 22:04:47 -0700

    started work on communication interface

---
+++ src/types.h
@@ -0,0 +1,14 @@
+//
+// Created by Fedorov, Alex on 2/28/18.
+//
+
+#ifndef MARKOS_TYPES_H
+#define MARKOS_TYPES_H
+using int8 = char;
+using int16 = short int;
+using int32 = int;
+using uint8 = unsigned char;
+using uint16 = unsigned short int;
+using uint32 = unsigned int;
+#endif //MARKOS_TYPES_H
+
